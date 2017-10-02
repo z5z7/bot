54c3b2f57c8d79096f3c74655be0f2a40c2d7bdf
Bulat Nugmanov <bbnugmanov@ualberta.ca>
2017-10-01 18:19:17 -0700

    updated grunt and package config for live development

--- hsbc-api/gruntfile.js
+++ hsbc-api/gruntfile.js
@@ -12,13 +12,53 @@ module.exports = function (grunt) {
                     fast: 'never'
                 }
             }
+        },
+
+        watch: {
+            scripts: {
+                files: ["src/\*\*/\*.ts", "!node_modules/\*\*/\*.ts"],
+                tasks: ["newer:tslint:all", "ts:build"],
+                options: {spawn: false}
+            }
+        },
+
+        nodemon: {
+            dev: {
+                script: 'out/www.js'
+            },
+            options: {
+                ignore: ['node_modules/**', 'gruntfile.js'],
+                env: {PORT: '8181'}
+            }
+        },
+
+        tslint: {
+            options: {
+                configuration: grunt.file.readJSON("src/tslint.json")
+            },
+            all: {
+                src: ["src/\*\*/\*.ts", "!node_modules/\*\*/\*.ts"]
+            }
+        },
+
+        concurrent: {
+            watchers: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
         }
 
     });
 
     grunt.loadNpmTasks("grunt-ts");
-    grunt.registerTask("build", [
-        "ts"
-    ])
+    grunt.loadNpmTasks("grunt-tslint");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-nodemon");
+    grunt.loadNpmTasks("grunt-concurrent");
+
+    grunt.registerTask("build", ["ts"]);
+    grunt.registerTask("serve", ["concurrent:watchers"]);
 
 };
