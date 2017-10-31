e7390133a04d02aab032f2498b9de3a220779178
Bowlboy <ws_12345@yahoo.com>
2017-10-30 18:45:56 -0700

    Updated FIND and SEARCH ATM Location (#9)
    TODO:
    * Refactor to grab ATM location from backend
    
    * Making the cases and handler stubs (#6)
    
    * update to google assistants stubs
    
    * add note for drawing google map with marker
    
    * add example for rich response like basic card
    
    * prompt user location
    give back map with user location mark
    still need the HSBC mark
    
    * FUNCTIONS REGARDING ATM LOCATION DONE (ONLY NEED TO SWITCH KEYS)
    
    * FUNCTIONS REGARDING ATM LOCATION DONE (ONLY NEED TO SWITCH KEYS)

--- src/actions.ts
+++ src/actions.ts
@@ -1,5 +1,17 @@
 export namespace Actions {
 
-    export const MY_ACTION = "my.action";
-
+    export const MY_ACTION = "my.action.trial";
+    export const WELCOME = "input.welcome.trial";
+    export const MORTGAGERATE_SPECIALOFFER_ADVANCE = "mortgageRate.specialOffer.advance";
+    export const MORTGAGERATE_SPECIALOFFER_PREMIER = "mortgageRate.specialOffer.premier";
+    export const MORTGAGERATE_SPECIALOFFER_PERSONALRATES = "mortgageRate.specialOffer.personalRates";
+    export const MORTGAGERATE_SPECIALOFFER_SMARTSAVER = "mortgageRate.specialOffer.smartSaver";
+    export const FIND_WHAT_EXCHANGERATE = "find.what.exchangeRate";
+    export const FIND_ATM = "find.atm";
+    export const FIND_ATM_FALLBACK = "actions.intent.PERMISSION";
+    export const SEARCH_CURRENCY_EXCHANGE = "search.currency.exchange";
+    export const SEARCH_WHAT_EXCHANGERATE = "search.what.exchangeRate";
+    export const SEARCH_WHAT_MORTGAGETYPE = "search.what.mortgageType";
+    export const SEARCH_WHERE_ATM = "search.where.atm";
+    export const SEARCH_WHAT_MORTGAGE_CALCULATOR_PAYMENT_LEFT = "search.what.mortgageCalculator.paymentsLeft";
 }
