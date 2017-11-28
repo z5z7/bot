bd01844597e24ed54ee218a8855ad94812ec95f7
jydenberg <joshydenberg@gmail.com>
2017-11-27 20:15:01 -0800

    bounds checks

--- src/Calculator.ts
+++ src/Calculator.ts
@@ -28,6 +28,11 @@ export namespace Calculator {
             let interestRate = req.body.result.parameters.interestRate;
             let loanDuration = req.body.result.parameters.loanDuration;
 
+
+            if (Number(loanAmount) < 0) reject ("Amount must be above 0");
+            if (Number(interestRate) < 0)reject ("Rate must be above 0");
+            if (Number(loanDuration) < 0)reject ("Loan Duration must be above 0");
+
             let arg = "0001/?amount=" + loanAmount.toString() + "&interestRate=" + interestRate + "&years=" + loanDuration.toString();
 
             client.calculateProductIdGet("loans", arg).then(result => {
@@ -54,6 +59,11 @@ export namespace Calculator {
             let loanDuration = params.loanDuration;
             let numberPayments = params.numberPayments;
 
+            if (Number(loanAmount) < 0) reject ("Amount must be above 0");
+            if (Number(interestRate) < 0)reject ("Rate must be above 0");
+            if (Number(loanDuration) < 0)reject ("Loan Duration must be above 0");
+            if (Number(numberPayments) < 0) reject ("Nummber Payments must be above 0");
+            if (Number (numberPayments) > ((Number(loanDuration)) * 12)) reject("More payments then months in duration");
             let arg = "0002/?amount=" + loanAmount.toString() + "&interestRate=" + interestRate.toString() + "&years=" + loanDuration.toString() + "&monthsRemaining=" + numberPayments.toString();
 
             client.calculateProductIdGet("loans", arg).then(result => {
