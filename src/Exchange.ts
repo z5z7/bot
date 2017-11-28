c4bf8bc4278eaeb320b34241b514129e5a7d564a
jydenberg <joshydenberg@gmail.com>
2017-11-27 23:34:14 -0800

    formatting and checking update

--- src/Exchange.ts
+++ src/Exchange.ts
@@ -57,11 +57,11 @@ export namespace Exchange {
 
                 let rateinfo = result.body.rates;
                 let Rarray = [];
-                Rarray.push("\nFrom Currency " + currency_from + ", the rates are \n\n");
+                Rarray.push("\nFrom Currency " + currency_from + ", the rates are ");
                 for (let i = 0; i < rateinfo.length; i++) {
                     let exchinfo = rateinfo[i];
                     let bprice = exchinfo.buy, sprice = exchinfo.sell, fromcode = exchinfo.code;
-                    let str1 = ("Buy price: "+  bprice.toString(),", Sell Price: ", + sprice.toString() + "Currency Code: " + fromcode.toString());
+                    let str1 = ("Currency Code: " + fromcode.toString() + " , Buy price: " +  bprice.toString() + ", Sell Price: " + sprice.toString());
                     Rarray.push(str1);
                 }
 
@@ -82,7 +82,7 @@ export namespace Exchange {
 
             if (!req.body.result) reject("invalid request");
             let currency_from = req.body.result.parameters.currency_from;
-            if (currency_from == "") currency_from = "CAD";
+            if (currency_from == "" || currency_from == undefined) currency_from = "CAD";
             client.xratesFromGet(currency_from).then(result => {
 
                 let rateinfo = result.body.rates;
@@ -91,13 +91,20 @@ export namespace Exchange {
                 for (let i = 0; i < rateinfo.length; i++) {
                     let exchinfo = rateinfo[i];
                     console.log("exchinfo: " + exchinfo);
-                    let bprice = exchinfo.buy, sprice = exchinfo.sell, fromcode = exchinfo.code;
-                    let str1 = ("Buy price: "+  bprice.toString()+ ", Sell Price: " + sprice.toString() + ",Currency Code: " + fromcode.toString());
+                    let bprice: string = exchinfo.buy.toFixed(3);
+                    let sprice: string = exchinfo.sell.toFixed(3);
+                    let fromcode = exchinfo.code;
+                    let str1 = " , Buy price: "+  bprice;
+                    let str2 = " , Sell Price: " + sprice;
+                    let str3 = "Currency Code: " + fromcode.toString();
+
+                    Rarray.push(str3);
+                    Rarray.push(str2);
                     Rarray.push(str1);
                     Rarray.push('\n');
                 }
 
-                let answer = Rarray.join('\n');
+                let answer = Rarray.join();
                 resolve(answer);
 
             }).catch(err => { // catch for promise loop
@@ -163,8 +170,8 @@ export namespace Exchange {
                 let len2 = result.body.rates.length;
                 for (j = 0; j < len2; j++) {
                     let str0 = "for " + cur;
-                    let str1 = str0.concat(" to ", result.body.rates[j].code," rate for buy is ", result.body.rates[j].buy.toString());
-                    let str2 = str0.concat(" ,sell rate is ", result.body.rates[j].sell.toString());
+                    let str1 = str0.concat(" to ", result.body.rates[j].code," rate for buy is ", result.body.rates[j].buy.toFixed(2));
+                    let str2 = str0.concat(" ,sell rate is ", result.body.rates[j].sell.toFixed(2));
                     Parray.push(str1);
                     Parray.push(str2);
                 }
